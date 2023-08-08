c3627f346a1ab964ded07825da3a0e0b8f724898
trannhantam4 <80136734+trannhantam4@users.noreply.github.com>
2023-08-08 13:09:35 +0700

    data

--- store/expenses-context.js
+++ store/expenses-context.js
@@ -8,7 +8,7 @@ export const ExpensesContext = createContext({
 });
 
 const DUMMY_EXPENSES = [
-  { id: 1, des: "shoes", price: 55.99, date: new Date("2021-12-19") },
+  { id: 1, des: "shoes", price: 55.99, date: new Date("2023-8-8") },
   { id: 2, des: "brace", price: 20.99, date: new Date("2021-12-12") },
   { id: 3, des: "braclet", price: 10.99, date: new Date("2021-12-12") },
   { id: 4, des: "braces", price: 3.99, date: new Date("2021-12-15") },
